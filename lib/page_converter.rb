e91fca86a20faa125c3108ad9116db6d27cb57d1
Mike Petersen <mike@odania-it.de>
2016-06-07 23:41:48 +0200

    Added prefix to partials

--- lib/page_converter.rb
+++ lib/page_converter.rb
@@ -30,12 +30,19 @@ class PageConverter
 
 			# Load partials.json
 			partial_file = File.join directory, 'partials.json'
+			prefix = "partial/#{domain}/#{subdomain}"
 			if File.exists? partial_file
 				partial_config = JSON.parse File.read(partial_file)
+
+				partial_config.each_pair do |key, value|
+					value['template'] = "#{prefix}/#{value['template']}"
+					partial_config[key] = value
+				end
+
 				$partials[domain][subdomain][:default].merge!(partial_config)
 			end
 
-			@page_processor.process_pages(directory, domain, subdomain, :partials)
+			@page_processor.process_pages(directory, domain, subdomain, :partials, prefix)
 		end
 	end
 end
